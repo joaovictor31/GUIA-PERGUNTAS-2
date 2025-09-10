a281932f77868144f874b5412dbbb082baabaea0
João <ut7vitor60@gmail.com>
2025-09-10 10:53:08 -0300

    Testando condicionais ejs

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const app = express();
 
 app.set('view engine','ejs');
 
-app.get("/:nome/:idade", (req,res) => {
+app.get("/:nome/:idade/:nota", (req,res) => {
     let nome = req.params.nome;
     let idade = req.params.idade;
+    let logado = true;
+    let nota = req.params.nota;
+    let temProdutos = true;
     res.render("index", {
         nome: nome,
         idade: idade,
-        altura: 1.70
+        altura: 1.70,
+        acesso: logado,
+        nota:nota,
+        estoqueProdutos: temProdutos
     });
 })
 
