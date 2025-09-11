dedbbbe0879569a0b5c9839acf420d6adde646a5
João <ut7vitor60@gmail.com>
2025-09-11 14:12:42 -0300

    Removendo exercícios antigos e adicionando bootstrap

--- index.js
+++ index.js
@@ -4,20 +4,8 @@ const app = express();
 app.set('view engine','ejs');
 app.use(express.static('public'));
 
-app.get("/:nome/:idade/:nota", (req,res) => {
-    let nome = req.params.nome;
-    let idade = req.params.idade;
-    let logado = true;
-    let nota = req.params.nota;
-    let temProdutos = true;
-    res.render("index", {
-        nome: nome,
-        idade: idade,
-        altura: 1.70,
-        acesso: logado,
-        nota:nota,
-        estoqueProdutos: temProdutos
-    });
+app.get("/", (req,res) => {
+    res.render("index");
 })
 
 app.listen(8080, function(){
